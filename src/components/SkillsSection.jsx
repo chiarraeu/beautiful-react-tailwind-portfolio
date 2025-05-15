8a23d672c5bbffc3e65711e7fb3ead6bb3bcab77
Chiarra <103839751+chiarraeu@users.noreply.github.com>
2025-05-15 20:59:43 +0300

    Update SkillsSection.jsx

--- src/components/SkillsSection.jsx
+++ src/components/SkillsSection.jsx
@@ -19,8 +19,9 @@ const skills = [
 
   // Tools
   { name: "Git/GitHub", level: 90, category: "tools" },
+  { name: "Jenkins", level: 75, category: "tools" } 
   { name: "Docker", level: 70, category: "tools" },
-  { name: "Figma", level: 85, category: "tools" },
+  { name: "Kubernetes", level: 85, category: "tools" },
   { name: "VS Code", level: 95, category: "tools" },
 ];
 
